c8b8af9f25484d9e70d4dcbddc271bc9b4e9d73d
claudinei312 <129322307+claudinei312@users.noreply.github.com>
2026-04-11 07:49:47 -0300

    Update robo.py

--- robo.py
+++ robo.py
@@ -1,5 +1,30 @@
 import streamlit as st
+from twelvedata import TDClient
+import pandas as pd
 
-API_KEY = st.secrets.get("API_KEY")
+API_KEY = st.secrets["4b17399dcf214533abd7d72ea416f1df"]
+td = TDClient(API_KEY)
 
-st.write("DEBUG API KEY:", API_KEY)
+ativo = "EUR/USD"
+intervalo = "5min"
+
+
+@st.cache_data(ttl=300)  # 🔥 ESSENCIAL
+def pegar_dados():
+    try:
+        ts = td.time_series(
+            symbol=ativo,
+            interval=intervalo,
+            outputsize=200
+        ).as_pandas()
+
+        ts = ts[::-1].reset_index(drop=True)
+
+        for col in ['open', 'high', 'low', 'close']:
+            ts[col] = pd.to_numeric(ts[col], errors='coerce')
+
+        return ts.dropna()
+
+    except Exception as e:
+        print("Erro API:", e)
+        return None
