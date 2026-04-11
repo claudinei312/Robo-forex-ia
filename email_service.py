653cc82e2edd04249fae994cf1685d6827135473
claudinei312 <129322307+claudinei312@users.noreply.github.com>
2026-04-11 07:39:42 -0300

    Update email_service.py

--- email_service.py
+++ email_service.py
@@ -1,17 +1,23 @@
+import streamlit as st
 import smtplib
 from email.mime.text import MIMEText
 
-EMAIL = "claudineialvesjunior@gmail.com"
-SENHA = "gbin ugpq tosj owxf"
+EMAIL = st.secrets["claudineialvesjunior@gmail.com"]
+SENHA = st.secrets["gbin ugpq tosj owxf"]
+
 
 def enviar_email(msg):
-    mensagem = MIMEText(msg)
-    mensagem["Subject"] = "Robô Forex"
-    mensagem["From"] = EMAIL
-    mensagem["To"] = EMAIL
+    try:
+        email = MIMEText(msg)
+        email["Subject"] = "🤖 Robô Forex IA"
+        email["From"] = EMAIL
+        email["To"] = EMAIL
 
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(EMAIL, SENHA)
-    server.send_message(mensagem)
-    server.quit()
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(EMAIL, SENHA)
+        server.send_message(email)
+        server.quit()
+
+    except Exception as e:
+        print("Erro email:", e)
