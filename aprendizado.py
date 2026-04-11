72ac82f9f4917c57ad850b9c3362ca1cf8e659f2
claudinei312 <129322307+claudinei312@users.noreply.github.com>
2026-04-11 07:38:11 -0300

    Update aprendizado.py

--- aprendizado.py
+++ aprendizado.py
@@ -1,13 +1,15 @@
 import csv
+from datetime import datetime
 
 def salvar_trade(rsi, tipo):
     with open("trades.csv", "a", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow([rsi, tipo])
+        writer.writerow([datetime.now(), rsi, tipo])
+
 
 def analisar_erro(rsi):
     if rsi > 65:
-        return "RSI muito alto"
+        return "RSI alto → risco de reversão"
     elif rsi < 35:
-        return "RSI muito baixo"
-    return "Mercado instável"
+        return "RSI baixo → possível sobrevenda"
+    return "Mercado neutro"
